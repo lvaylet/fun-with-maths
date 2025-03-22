06e1fc24890533e3207a9a62a60d3d3430b29618
Laurent VAYLET <laurent.vaylet@gmail.com>
2025-03-22 18:20:27 +0100

    listen on 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -90,5 +90,5 @@ async fn main() {
 
     let routes = index.or(question_route).or(answer_route);
 
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
 }
